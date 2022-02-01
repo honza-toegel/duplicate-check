2782656b945ca44b508421f270d1a541b1b2c6dd
honza.toegel <honza.toegel@gmail.com>
2022-02-01 05:10:38 +0100

    small cleanup

--- duplicate-check-rest-api/src/main/kotlin/org/jto/dupcheck/SecurityConfiguration.kt
+++ duplicate-check-rest-api/src/main/kotlin/org/jto/dupcheck/SecurityConfiguration.kt
@@ -31,7 +31,6 @@ class SecurityConfiguration() : WebSecurityConfigurerAdapter(true) {
             .and()
             .authorizeRequests()
             .antMatchers(HttpMethod.GET, "/").hasAnyRole("GUEST")
-            .antMatchers(HttpMethod.GET, "/test").hasAnyRole("GUEST")
             .and()
             .cors().and()
             .csrf().disable()
